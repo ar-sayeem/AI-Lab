7f49ff2eb1f2c8a80c0eaa20b16fbdd01acaae71
Adnan-Mini <sayeem15-5505@diu.edu.bd>
2025-02-23 15:39:45 +0600

    up t1t2

--- test-1.py
+++ test-1.py
@@ -1,1 +1,51 @@
-print("Hello")
+def dfs_total_cost(graph):
+    total_cost = sum(sum(edges.values()) for edges in graph.values())
+    return total_cost
+
+
+def dfs(graph, start):
+    stack = [(0, start, [start])]  # (cost, node, path)
+    paths = []
+
+    while stack:
+        cost, current, path = stack.pop()
+
+        if not graph[current]:  # If leaf node
+            paths.append((cost, path))
+
+        for neighbor, edge_cost in graph[current].items():
+            stack.append((cost + edge_cost, neighbor, path + [neighbor]))
+
+    # print("All paths to leaf nodes:")
+    # for cost, path in paths:
+    #     print("Path:", path, "Cost:", cost)
+
+    # Find and print the shortest path
+    if paths:
+        min_cost, min_path = min(paths)  # Automatically sorts by cost
+        print("\nMinimum cost path:", min_path, "with cost:", min_cost)
+    else:
+        print("No valid path found.")
+
+
+graph = {
+    'A': {'B': 1, 'C': 2},
+    'B': {'D': 1, 'E': 2},
+    'C': {'F': 3, 'G': 4},
+    'D': {'H': 2, 'I': 1},
+    'E': {'J': 4, 'K': 3},
+    'F': {'L': 2, 'M': 3},
+    'G': {'N': 1, 'O': 1},
+    'H': {},
+    'I': {},
+    'J': {},    
+    'K': {},
+    'L': {},
+    'M': {},
+    'N': {},
+    'O': {}
+}
+
+total_cost = dfs_total_cost(graph)
+print("Total cost of the tree:", total_cost)
+dfs(graph, 'A')
---
+++ test-2.py
@@ -0,0 +1,50 @@
+def dfs_total_cost(graph):
+    total_cost = 0
+    for node in graph:
+        total_cost += sum(graph[node].values())
+    return total_cost
+
+
+def dfs_min_cost_path(graph, start):
+    stack = [(start, [start], 0)]
+    min_cost = float('inf')
+    min_path = []
+    
+    while stack:
+        node, path, cost = stack.pop()
+        
+        if not graph[node]:
+            if cost < min_cost:
+                min_cost = cost
+                min_path = path
+        
+        for neighbor, edge_cost in graph[node].items():
+            stack.append((neighbor, path + [neighbor], cost + edge_cost))
+    
+    return min_cost, min_path
+
+
+
+graph = {
+    'A': {'B': 1, 'C': 2},
+    'B': {'D': 1, 'E': 2},
+    'C': {'F': 3, 'G': 4},
+    'D': {'H': 2, 'I': 1},
+    'E': {'J': 4, 'K': 3},
+    'F': {'L': 2, 'M': 3},
+    'G': {'N': 1, 'O': 1},
+    'H': {},
+    'I': {},
+    'J': {},    
+    'K': {},
+    'L': {},
+    'M': {},
+    'N': {},
+    'O': {}
+}
+
+total_cost = dfs_total_cost(graph)
+min_cost, min_path = dfs_min_cost_path(graph, 'A')
+
+print("Total cost of the tree:", total_cost)
+print("Minimum cost path:", min_path, "Cost:", min_cost)
